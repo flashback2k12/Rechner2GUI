c9fc9b821aa0072a04f808b7b1ea3dff51bcf624
flashback <sebastian-kloppe@gmx.de>
2012-11-16 09:25:27 +0100

    new operations

--- src/Main/CalcOperator.java
+++ src/Main/CalcOperator.java
@@ -9,4 +9,30 @@ public class CalcOperator {
 		return ergebnis; 
 	}
 	
+         /*
+         * new Operations
+         */
+
+         public static double sub2 (double zahl1, double zahl2) {
+		
+		double ergebnis = zahl1 - zahl2;
+	
+		return ergebnis; 
+	}
+
+         public static double mul2 (double zahl1, double zahl2) {
+		
+		double ergebnis = zahl1 * zahl2;
+	
+		return ergebnis; 
+	}
+
+         public static double div2 (double zahl1, double zahl2) {
+		
+		double ergebnis = zahl1 / zahl2;
+	
+		return ergebnis; 
+	}
+
+
 }
